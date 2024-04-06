24b9c30fdb19da4a85e7b079614c67dd75c83bca
Ali <Alihunter4640@gmail.com>
2024-04-06 17:19:38 +0330

    Composer's pieces page
    url , media , view , and template made and fastened to list and show a composer's masterpieces

--- music/urls.py
+++ music/urls.py
@@ -5,4 +5,5 @@ app_name = "music"
 
 urlpatterns = [
    path('composers',ComposersView , name="composers" ),
+   path('pieces/<int:cid>' , ComposerPieces , name="pieces"),
 ]
--- music/views.py
+++ music/views.py
@@ -1,9 +1,33 @@
 from django.shortcuts import render
-from .models import Composer 
+from .models import Composer, MasterPiece 
+from django.shortcuts import get_object_or_404
+from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
 
 
 
 def ComposersView(request):
     composers = Composer.objects.all()
+
+    p = Paginator(composers,8)
+    try:
+        page_number = request.GET.get('page')
+        composers = p.get_page(page_number)
+    except EmptyPage:
+        composers = composers.get_page(1)
+    except PageNotAnInteger:
+        composers = composers.get_page(1)
+
     context = {"composers": composers}
     return render(request,'music/composers.html',context)
+
+
+
+
+def ComposerPieces(reqeust,cid):
+    all_composers = Composer.objects.all()
+    current_composer = get_object_or_404(all_composers , pk = cid) 
+    pieces = MasterPiece.objects.filter(composer = cid)
+    context = {"composer" : current_composer , "pieces" : pieces }
+    return render (reqeust,'music/masterpieces.html',context)
+
+
