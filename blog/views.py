3299b24a426d8dc29b7fc46c5218fbfc11bf1bce
Ali <Alihunter4640@gmail.com>
2024-04-08 12:32:12 +0330

    Prev and Next post buttons
    Prev and Next Buttons for Blog's single post made by using Django's get context data method into Django's detailview class

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+from typing import Any
 from django.shortcuts import render
 from .models import Post 
 from django.views import generic
@@ -16,3 +17,15 @@ class PostList(generic.ListView):
 class DetailView(generic.DetailView):
     model = Post 
     template_name = "blog/blog-single.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        post = self.get_object()
+        # Get The previous post 
+        previous_post = Post.objects.filter(id__lt=post.id).order_by('-id').first()
+        # Get the next post 
+        next_post = Post.objects.filter(id__gt=post.id).order_by('id').first()
+        context['previous_post'] = previous_post 
+        context['next_post'] = next_post 
+        return context 
+    
